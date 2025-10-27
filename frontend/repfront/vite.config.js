3ce12571732581fa06676e6e6e0356bec2cf67a8
tkt0605 <takato.komada2005@outlook.jp>
2025-10-28 08:02:07 +0900

    vite.config.jsでのVite設定の変更

--- frontend/repfront/vite.config.js
+++ frontend/repfront/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: false,   // ← 明示的に無効
+    },
+    hmr:{
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5173,
+      clientPort: 5173
+    },
+  },
 })
